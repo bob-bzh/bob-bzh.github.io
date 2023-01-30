033e339cc74ca6bf9ba23bb92d1e1a9c6ae9de1d
bobovo <2950253898@qq.com>
2023-01-30 21:01:59 +0800

    Site updated: 2023-01-30 21:01:54

---
+++ js/weather.js
@@ -0,0 +1,44 @@
+/* WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "16",
+    "cityColor": "CCCCCC",
+    "citySize": "16",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "15px 10px 10px 20px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "...."
+  }
+}   fomalhaut*/
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "2",
+    "tmpColor": "666666",
+    "tmpSize": "16",
+    "cityColor": "666666",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "7px 7px 7px 7px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "10",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "16ba1c7e14434b0f81cc7e991225846b"
+  }
+}
